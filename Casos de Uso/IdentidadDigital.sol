9a12ef996760255712388e1e10497bfb36abff16
phoenixdhr <phoenixdhr@gmail.com>
2022-12-02 15:45:05 -0500

    Se creo un contrato de IdentidadDigital para Autentificar cuentas

--- Casos de Uso/IdentidadDigital.sol
+++ Casos de Uso/IdentidadDigital.sol
@@ -2,5 +2,13 @@
 pragma solidity >=0.7.0  <=0.9.0 ;
 
 contract Name {
+
+    mapping (address=>bool) public users;
+
+    function authenticate () public {
+        users[msg.sender]=true;
+    }
+
+
     
 }
