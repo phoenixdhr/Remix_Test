555e907b8d914a27717414dd36d7703f5842dd64
phoenixdhr <phoenixdhr@gmail.com>
2023-02-07 14:36:29 -0500

    Se adjunt archivo.sol que consta de tres contratos, un contrato que puede llamar a un contrato "Count" o tambien puede llamar a la misma funcion usnado un contrato tipo proxy que usa un fallback

---
+++ DescubiendoTest/Fallback _Call.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >0.8.10;
+
+
+contract inputOutputFallback {
+
+    address immutable addressCounter;
+
+    constructor(address _addressCounter) {
+        addressCounter = _addressCounter;
+    }
+
+    fallback(bytes calldata req) external payable returns(bytes memory) {
+            
+            (bool ok, bytes memory res)= addressCounter.call{value: msg.value}(req);
+            require(ok, "call failed");
+
+            return res;
+    }
+}
+
+contract Counter {
+    uint public count;
+
+    function inc() public  returns(uint) {
+        count+=1;
+        return count;
+    }
+    
+    receive() external payable{
+    }
+}
+
+
+
+contract TestFallbackInputOutput {
+    
+    function singFuncInc() public pure returns (bytes memory ) {
+        bytes memory firmaFunInc = abi.encodeCall(Counter.inc, ());
+        return firmaFunInc;
+    }
+
+    function sendCall(address _to, bytes calldata firmaFunInc) public returns(bytes memory) {
+        bytes memory req = firmaFunInc;
+        (bool ok, bytes memory res) = _to.call(req);
+        require(ok, "call failed");
+        return res;
+    }
+}
